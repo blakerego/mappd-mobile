7ddda2f3dd371bf8caa321b8a2c19ddf83618998
blake <blake.rego@gmail.com>
2016-12-15 20:04:21 -0500

    Adding sass to project.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'mappd' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('mappd', ['ionic'])
+angular.module('mappd', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
