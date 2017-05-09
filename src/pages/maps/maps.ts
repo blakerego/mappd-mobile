bac3fe5a1a55b6bcc341cea9fbd9e36171e7be4a
Blake Rego <blake.rego@gmail.com>
2017-05-09 00:29:16 -0400

    Loading maps onto maps page.

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-maps',
   templateUrl: 'maps.html'
 })
 export class MapsPage {
-
-  constructor(public navCtrl: NavController) {
-
+  maps = [];
+  constructor(public navCtrl: NavController, private http: Http) {
+    let server = 'http://localhost:3000';
+    let endpoint = '/maps.json';
+    this.http.get(server + endpoint)
+      .map(response => response.json())
+      .subscribe(data => {
+				this.maps = data;
+      });
   }
 
 }
